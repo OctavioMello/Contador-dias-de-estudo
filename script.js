a310254f3c59e3e3fe84c2afad288cdd2e405ed3
Octávio Mello Covre de Souza <octaviomellocovredesouza@gmail.com>
2026-04-06 20:08:51 +0100

    feat: adiciona controle de datas e evita duplicidade de registros

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let estado = {
     diasEstudados: [],
 }
+
+//executarFuncoes(limparDados);
+
 let dadosSalvos = localStorage.getItem("dias");
 
     if (dadosSalvos) {
@@ -9,7 +12,16 @@ let dadosSalvos = localStorage.getItem("dias");
 renderizar();
 
 function cliqueDoBotao(){
-    contarDia();
+    executarFuncoes(contarDia);
+}
+
+function limparDados(){
+    localStorage.removeItem("dias");
+    estado.diasEstudados = [];
+}
+
+function executarFuncoes(acao){
+    acao();
     renderizar();
 }
 
@@ -20,12 +32,19 @@ function renderizar(){
     nome.innerHTML = '';
     estado.diasEstudados.forEach(dias =>{
         const li = document.createElement('li');
-        li.textContent = (`Dia ${dias} de estudos.`);
+        li.textContent = (`Dia ${dias.dia} de estudos\n Data ${dias.data}`);
         nome.appendChild(li);
     });
 }
 
 function contarDia(){
-    estado.diasEstudados.push(estado.diasEstudados.length + 1);
+    let hoje = new Date().toLocaleDateString()
+    const jaRegistradoHoje = estado.diasEstudados.some(diasExistentes => diasExistentes.data == hoje);
+    if (!jaRegistradoHoje){
+            estado.diasEstudados.push({
+                dia : estado.diasEstudados.length + 1,
+                data : hoje,
+    })
+    }
     localStorage.setItem("dias", JSON.stringify(estado.diasEstudados));
 }
